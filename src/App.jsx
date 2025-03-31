3859379d22c4ce307a8f86923c87554252519b58
Guna Sekaran M <79622650+guna-sekar-mano@users.noreply.github.com>
2025-04-01 04:39:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 const App = () => {
   return (
     <div>
-      <h2 id="text-center">Spatial_Distribution_of_NO2_and_PM2.5</h2>
+      <h2 id="text-center">Spatial Distribution of NO2 and PM2.5</h2>
       <MapComponent />
     </div>
   );
